0a94d146f590c38a1ec3500ca7f210633e0ddcfb
Divyansh Chauhan <divyansh20060@gmail.com>
2024-12-31 19:39:43 +0530

    Test first apis

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const WishEmailRoute = require('./routes/KCMT-SIS/WishEmail/wishEmailRoute');
 // Set Path for All Applications-------------------------->
 
 
-app.use('/api/send-wish-email', WishEmailRoute);
+app.use('/duggu-api', WishEmailRoute);
 
 
 // Root API endpoint
