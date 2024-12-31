26f36e6285683efca9a38ac5563fc6b753fedd0d
Divyansh Chauhan <divyansh20060@gmail.com>
2024-12-31 19:55:46 +0530

    as

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const WishEmailRoute = require('./routes/KCMT-SIS/WishEmail/wishEmailRoute');
 // Set Path for All Applications-------------------------->
 
 
-app.use('/duggu-api', WishEmailRoute);
+app.use('/api', WishEmailRoute);
 
 
 // Root API endpoint
