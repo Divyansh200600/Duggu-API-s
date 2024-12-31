640b360bba848a620a0aa8fe6921dd20f2b4bc35
Divyansh Chauhan <divyansh20060@gmail.com>
2024-12-31 20:17:08 +0530

    Cors solveds

--- index.js
+++ index.js
@@ -4,49 +4,45 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const path = require('path'); 
 
-const corsOptions = {
-  origin: ['http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
-
 dotenv.config();
 
 const app = express();
 const port = 3001;
 
-app.use(bodyParser.json());
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-app.use(express.static(path.join(__dirname, 'public')));
+// Configure CORS options
+const corsOptions = {
+  origin: ['http://localhost:3000'], // Allow specific frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  credentials: true, // Allow credentials if needed
+};
 
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Allow preflight requests for all routes
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set Routes for All Applications------------------------>
 
 // 1. KCMT-SIS
-
 const WishEmailRoute = require('./routes/KCMT-SIS/WishEmail/wishEmailRoute');
 
 // 2. KCMT-DMS
-
 const DepartmentCreateRoute = require('./routes/KCMT-DMS/DepartmentCreate/departmentCreateRoute');
 
 // Set Path for All Applications-------------------------->
 
 // 1. KCMT-SIS
-
 app.use('/duggu-api', WishEmailRoute);
 
 // 2. KCMT-DMS
-
 app.use('/duggu-api', DepartmentCreateRoute);
-
 
 // Root API endpoint
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html')); 
 });
-
 
 // Start the server
 app.listen(port, () => {
